be921655157a881a16e323070df59db5a3674353
aweihmayerhamel <aweihmayerhamel@kezber.com>
2023-08-28 13:44:11 -0400

    update

---
+++ Utils/Dom/image.js
@@ -0,0 +1,19 @@
+﻿/**
+ * 
+ */
+HTMLImageElement.prototype.lazyload = () => {
+    if (this.src) { return; }
+    if (!this.dataset.src) { return; }
+    this.src = this.dataset.src;
+};
+
+HTMLPictureElement.prototype.lazyload = () => {
+    this.getImages().foreach(v => { v.lazyload(); });
+};
+
+/**
+ * Gets all image child elements.
+ */
+Element.prototype.getImages = function () {
+    this.querySelectorAll('img, picture');
+};
