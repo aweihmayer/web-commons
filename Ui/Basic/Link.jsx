1373609b39a8f20906b33f7178a2b5667e72f53d
aweihmayer <a.weihmayer@gmail.com>
2023-09-21 09:21:58 -0400

    update

--- Ui/Basic/Link.jsx
+++ Ui/Basic/Link.jsx
@@ -7,6 +7,11 @@
             href = this.props.route.uri.relative(this.props.params);
         }
 
-        return <a onClick={Router.goTo} href={href}>{this.props.children}</a>;
+        return <a
+            onClick={Router.goTo}
+            className={document.buildClassName(this.props.className)}
+            href={href}>
+            {this.props.children}
+        </a>;
     }
 }
